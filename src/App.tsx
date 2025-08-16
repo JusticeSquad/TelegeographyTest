428c17facc28df73f9fc7e13672ace2e982e261e
JusticeSquad <pjusteson@gmail.com>
2025-08-16 02:09:52 -0400

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <main>
+        <div>Hello World</div>
+    </main>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: isProduction ? "[name].[contenthash].js" : "[name].js",
+      clean: true,
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "postcss-loader",
+          ],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/,
+          type: "asset/resource",
+        },
+        {
+          test: /\.txt$/,
+          type: "asset/source",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      ...(isProduction
+        ? [
+            new MiniCssExtractPlugin({
+              filename: "[name].[contenthash].css",
+            }),
+          ]
+        : []),
+    ],
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+    devtool: isProduction ? "source-map" : "eval-source-map",
+  };
+};
